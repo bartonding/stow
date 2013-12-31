6cc34bcef567557a97136f9bd36174e686c2b234
dingzq <dingzq@rd.netease.com>
2013-12-31 19:42:30 +0800

    prolayout to flexlayout

---
+++ flexlayout/examples/test-module.js
@@ -0,0 +1,85 @@
+(function (root) {
+
+    function heredoc(fn) {
+        return fn.toString()
+            .replace(/^[^\/]+\/\*!?/, '')
+            .replace(/\*\/[^\/]+$/, '')
+            .replace(/^\s+/, '')
+            .replace(/\s+$/, '');
+    }
+    var testString = heredoc(function(){/*
+        <div class="mod" style="background-color:{{color}};width:100%;height:100%;">{{content}}</div>
+    */});
+    var generateString = function (content, color) {
+        return testString.replace('{{content}}', content).
+            replace('{{color}}', color);
+    };
+
+
+    // -------------------------------------------------------------------------
+    // -- test module
+    // -------------------------------------------------------------------------
+    root.testModule1 = (function () {
+        var mod = function () {};
+
+        mod.render = function () {
+            // return generateString('test module 1', 'red');
+            return generateString('<div style="height:100px;">111</div>', 'red');
+        };
+        mod._layoutResize = function (rs, ws, ctx) {
+            console.log('mod1 ......');
+            ctx.find('div.mod').html([JSON.stringify(rs), JSON.stringify(ws)].join('<br/>'));
+        };
+
+        return mod;
+    })();
+
+    root.testModule2 = (function () {
+        var mod = function () {};
+
+        mod.render = function () {
+            return generateString('test module 22', 'yellow');
+        };
+        mod._layoutResize = function (rs, ws, ctx) {
+            console.log('mod2 ......');
+            // console.log('mod2 ', arguments);
+            // console.log(mod.region);
+            ctx.find('div.mod').html([JSON.stringify(rs), JSON.stringify(ws)].join('<br/>'));
+        };
+
+        var xx = 0;
+        mod.xxrun = function () {
+            xx++;
+            // console.log('xxrun: ', xx++);
+            if (!mod.region) return;
+            var region = mod.region;
+            var m = region.layout.regions['a1'].context.find('div.mod');
+            // console.log(m);
+            // m.height((xx%2===1)?30:100);
+            m.width((xx%2===1)?30:100);
+            region.updateLayoutSize && region.updateLayoutSize();
+        };
+
+        return mod;
+    })();
+    // window.setInterval(testModule2.xxrun, 1000);
+    // window.setTimeout(testModule2.xxrun, 1000);
+
+    root.testModule3 = (function () {
+        var mod = function () {};
+
+        var node = null;
+
+        mod.render = function () {
+            return generateString('test module 333', 'green');
+        };
+        mod._layoutResize = function (rs, ws, ctx) {
+            console.log('mod3 ......');
+            // console.log('mod3 ',arguments);
+            ctx.find('div.mod').html([JSON.stringify(rs), JSON.stringify(ws)].join('<br/>'));
+        };
+
+        return mod;
+    })();
+
+})(this);
---
+++ flexlayout/flex-layout.js
@@ -0,0 +1,329 @@
+(function () {
+
+    var root = this;
+
+    var FlexLayout = function () {};
+
+    // 保持当前页面中的所有 layout 实例
+    var layoutCache = FlexLayout.layoutCache = {};
+
+    FlexLayout.create = function (config) {
+        var layout = new Layout(config);
+        layoutCache[layout.name] = layout;
+        return layout;
+    };
+
+    // -------------------------------------------------------------------------
+    // -- 生产全局 ID
+    var _layout_id = 0;
+    function generateLayoutId () {
+        return 'youdao_flexlayout_' + (_layout_id++);
+    }
+    var _region_id = 0;
+    function generateRegionId (layoutName) {
+        return layoutName + '_' + (_region_id++);
+    }
+
+    // -------------------------------------------------------------------------
+    // -- custom event
+    var ievent = (function () {
+        var slice = Array.prototype.slice;
+        var callbacks = {};
+        var splitEventString = function (str) {
+            return str.replace(/^\s+/,'').replace(/\s+$/,'').split(/\s+/);
+        };
+        var iEvent = {
+            on: function (etype, fn) {
+                if (!callbacks[etype]) {
+                    callbacks[etype] = [];
+                }
+                callbacks[etype].push(fn);
+            },
+            trigger: function () {
+                if (arguments.length === 0) return;
+                var events = splitEventString(arguments[0]);
+                var args = slice.call(arguments, 1);
+                _.each(events, function (etype) {
+                    _.each(callbacks[etype] || [], function (fn) {
+                        window.setTimeout(function () {
+                            fn.apply(null, args);
+                        }, 1);
+                    });
+                });
+            }
+        };
+        return iEvent;
+    })();
+    // 把自定义事件挂到 FlexLayout 上, 共外部使用
+    FlexLayout.on = ievent.on;
+    FlexLayout.trigger = ievent.trigger;
+
+    // -------------------------------------------------------------------------
+    // -- window resize 的处理
+    function layoutResize (evt) {
+        console.log('layout resize ......');
+        var w = $(window).width();
+        var h = $(window).height();
+        _.each(layoutCache, function(layout, layoutName) {
+            if (layout.isSubLayout) return;
+            ievent.trigger(layoutName, {width: w, height: h}, layoutName);
+        });
+    }
+    $(window).resize(_.debounce(layoutResize, 100));
+
+    // -------------------------------------------------------------------------
+    // -- Layout
+    // -------------------------------------------------------------------------
+    var DEFAULT_LAYOUT_CONFIG = {
+        // region 之间的距离
+        // vgap   : 0,
+        // hgap   : 0,
+        width  : '100%',
+        height : '100%',
+        // 配置 region 参数
+        scheme : [],
+        // 布局方式：row - 水平，column - 垂直
+        type   : 'row'
+    };
+
+    var Layout = function (conf) {
+        this.name = generateLayoutId();
+        this.isSubLayout = false; // 默认为非嵌套layout
+        this.conf = _.extend({}, DEFAULT_LAYOUT_CONFIG, conf || {});
+        this.regions = {};
+        this.init();
+    };
+
+    Layout.prototype = {
+        init: function () {
+            // layout 容器
+            this.wrap = generateDIV('wrap');
+            this.wrap.attr('id', this.name);
+
+            this.isRow = this.conf.type === 'row';
+
+            this.initLayoutContainer();
+
+            ievent.on(this.name, _.bind(this.layoutResize, this));
+        },
+        initLayoutContainer: function () {
+            // 初始化容器样式
+            this.wrap.css({
+                'width'  : this.conf.width,
+                'height' : this.conf.height
+            });
+
+            var self = this;
+            var regions = this.regions;
+
+            var trRow = generateTR();
+            var table = generateTABLE();
+
+            // 创建 region 实例
+            _.each(this._parseScheme(), function (rconf, idx) {
+                var region = new Region(rconf, self);
+                regions[rconf.name] = region;
+                if (self.isRow) {
+                    trRow.append(region.render());
+                } else {
+                    table.append(generateTR().append(region.render()));
+                }
+            });
+
+            if (self.isRow) table.append(trRow);
+
+            this.wrap.append(table);
+        },
+        // ws = Window Size = {width, height};
+        layoutResize: function (ws, evtName) {
+            console.log('at layout ', evtName);
+            _.each(this.regions, function (region, regionName) {
+                if (region.conf.type === 'max') {
+                    ievent.trigger(region.name, ws, region.name);
+                }
+            });
+        },
+        getRegion: function (regionName) {
+            return this.regions[regionName];
+        },
+        addModules: function (mods, regionName) {
+            var self = this;
+            _.each(mods, function(mod, idx) {
+                self.addModule(mod, regionName);
+            });
+        },
+        addModule: function (mod, regionName) {
+            var region = this.getRegion(regionName);
+            // 如果指定了不存在的 region，则不处理，容错
+            region && region.addModule(mod);
+        },
+        layout: function (node) {
+            var node = $(node);
+            if (node.length === 0) node = $(document.body);
+            // node.prepend(this.wrap);
+            node.prepend(this.wrap);
+        },
+        _parseScheme: function () {
+            var result = [], hasMaxType = false;
+            _.each(this.conf.scheme, function (region, idx) {
+                // 1、region 中嵌入 layout 实例, 忽略之
+                if (region instanceof Layout) return;
+
+                // 2、指定了 region 的配置参数
+                if (_.isObject(region)) {
+                    result.push(region);
+                    if (!hasMaxType && region.type === 'max') {
+                        hasMaxType = true;
+                        return;
+                    }
+                    // 避免出现多个 region.conf.type='max' 的情况
+                    if (hasMaxType && region.type === 'max') {
+                        region.type = 'min';
+                    }
+                }
+                // 3、仅指定了 region 名称
+                else if (_.isString(region)) {
+                    result.push({'name': region});
+                }
+            });
+
+            // 确保每个layout中至少包含一个 type='max' 类型
+            // 默认为最后一个 region
+            if (!hasMaxType && result.length > 0) {
+                result[result.length - 1].type = 'max';
+            }
+
+            this.conf.scheme = result;
+
+            return result;
+        }
+    };
+
+    // -------------------------------------------------------------------------
+    // -- Region
+    // -------------------------------------------------------------------------
+    var DEFAULT_REGION_CONFIG = {
+        // ['fixed', 'min', 'max']
+        type: 'fixed',
+        size: 0
+    };
+
+    var Region = function (conf, layout) {
+        var cf = this.conf = _.extend({}, DEFAULT_REGION_CONFIG, conf || {});
+
+        var size = parseInt(cf.size, 10);
+        cf.size = _.isNaN(size) ? 0 : size;
+        if (cf.size === 0 && cf.type === 'fixed') cf.type = 'min';
+
+        this.name = generateRegionId(layout.name);
+        this.layout = layout;
+        this.mods = [];
+        this.init();
+    };
+
+    Region.prototype = {
+        init: function () {
+            var conf = this.conf;
+            var layout = this.layout;
+
+            this.wrap = generateTD();
+            this.context = generateDIV('region');
+            this.context.attr('id', this.name);
+            this.wrap.append(this.context);
+
+            // 初始化 region 样式
+            var attrName = layout.isRow ? 'width' : 'height';
+            var type = conf.type;
+            if (type === 'max') {
+                this.wrap.css(attrName, '100%');
+                this.context.css(attrName, '100%');
+            } else if (type === 'fixed') {
+                this.context.css(attrName, conf.size);
+            }
+
+            if (layout.isRow) {
+                this.context.css('height', layout.conf.height);
+            }
+
+            ievent.on(this.name, _.bind(this.regionResize, this));
+        },
+        // ws = Window Size = {width, height};
+        regionResize: function (ws, evtName) {
+            console.log('at region ', evtName);
+            var self = this;
+            var ctx = this.context;
+            _.each(this.mods, function (mod, idx) {
+                if (mod instanceof Layout) {
+                    ievent.trigger(mod.name, ws, mod.name);
+                } else {
+                    _.isFunction(mod._layoutResize) && mod._layoutResize(
+                        {width: ctx.width(), height: ctx.height()},
+                        ws, self.context
+                    );
+                }
+            });
+        },
+        // region 中的module可主动调用该方法触发layout resize事件
+        updateLayoutSize: function () {
+            var w = $(window).width();
+            var h = $(window).height();
+            var lname = this.layout.name;
+            ievent.trigger(lname, {width: w, height: h}, lname);
+        },
+        addModules: function (mods) {
+            var self = this;
+            _.each(mods || [], function (mod, idx) {
+                self.addModule(mod);
+            });
+        },
+        addModule: function (mod) {
+            mod.region = this;
+            this.mods.push(mod);
+
+            if (mod instanceof Layout) {
+                mod.isSubLayout = true;
+                mod.wrap.css({width: '100%', height: '100%'});
+                mod.layout(this.context);
+            } else { this.context.append(mod.render()); }
+        },
+        render: function () {
+            return this.wrap;
+        }
+    };
+
+    // -------------------------------------------------------------------------
+    // Class Name Prefix
+    var CNP = 'youdao-flexlayout-';
+
+    function generateDIV(className) {
+        return $('<div class="' + CNP + className + '">');
+    }
+    function generateTD() {
+        return $('<td class="' + CNP + 'td" style="vertical-align:top;height:100%;">');
+    }
+    function generateTR() {
+        return $('<tr class="' + CNP + 'tr">');
+    }
+    function generateTABLE() {
+        return $([
+            '<table class="' + CNP + 'table" ',
+                'style="width:100%;height:100%;border-collapse:collapse;" ',
+                'border="0" cellpadding="0" cellspacing="0" ',
+            '>'
+        ].join(''));
+    }
+
+    // -------------------------------------------------------------------------
+    // 参考自 underscore.js
+    // Export the UA object for **Node.js**, with
+    // backwards-compatibility for the old `require()` API. If we're in
+    // the browser, add `_` as a global object via a string identifier,
+    // for Closure Compiler "advanced" mode.
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = FlexLayout;
+        }
+        exports.FlexLayout = FlexLayout;
+    } else { root.FlexLayout = FlexLayout; }
+
+}).call(this);
